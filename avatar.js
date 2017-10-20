67b9325096f57d5c2027db395885ff5129750f57
ryguy4 <31867251+ryguy4@users.noreply.github.com>
2017-10-19 18:47:20 -0600

    Update avatar.js

--- avatar.js
+++ avatar.js
@@ -1,8 +1,13 @@
 angular.module('app', [])
-  .controller('mainCtrl', mainCtrl)
-  .directive('avatar', avatarDirective);
+  .controller('mainCtrl', mainCtrl);
 
 function mainCtrl ($scope) {
+
+$scope.items = [];
+
+$scope.add = function () {
+  $scope.items.push({});
+};
 
   $scope.users = [];
 
@@ -17,29 +22,4 @@ function mainCtrl ($scope) {
     user.url = '';
 	user.email = '';
   };
-}
-
-function avatarDirective () {
-  return {
-    scope: {
-      user: '=' /* [1] */
-    },
-    restrict: 'E', /* [2] */
-    replace: 'true',
-    template: (
-      '<div class="Avatar">' +
-        '<img ng-src="{{user.avatarUrl}}" />' +
-        '<h4>{{user.name}}</h4>' +
-		'<h5><a href="mailto:{{user.email}}" target="_top">{{user.email}}</a> </h3>'+
-      '</div>'
-    ), /* [3] */
-    link: link
-  };
-  
-  function link (scope) { /* [4] */
-    if (!scope.user.avatarUrl) {
-      scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
-    }
-  }
-
 }
